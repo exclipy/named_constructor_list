979fec6f89f4b6cbf4fbbf4707981e0e35eae069
Kevin Wu Won <exclipy@gmail.com>
2012-07-29 00:23:00 +1000

    Initial Commit

---
+++ partial_constructor/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  partial_constructor
+//
+//  Created by Kevin Wu Won on 29/07/12.
+//  Copyright (c) 2012 Kevin Wu Won. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
